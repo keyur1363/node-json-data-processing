0806d0828b7d01663a1036c25a19b08d432563e7
Keyur Patel <Keyur Patel>
2016-07-27 20:55:48 -0700

    json hw webfront

---
+++ routes/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var course = req.query.course;
+    var result= null;
+   var data = {
+        "CS551": {
+            1:{
+            'homework_id': 1,
+            'score': 84
+            },
+            2: {
+                'homework_id': 2,
+                'score': 93
+            },
+            3: {
+                'homework_id': 3,
+                'score': 88
+            }
+        },
+        "CS557": {
+            1: {
+                'homework_id': 1,
+                'score': 90 
+            },
+            2: {
+                'homework_id': 2,
+                'score': 85
+            }
+        }
+    };
+    
+    //console.log(data.length);
+    for(var a in data){
+        if (a == course) {
+            result = data[a];
+            }
+    }
+   
+    if (result!=null) {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(result))
+    } else {
+        res.writeHead(404)
+        res.end("404----Course not found.")
+    }
+});
+
+module.exports = router;
